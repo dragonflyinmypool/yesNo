44abaa4430457afa98a0bd34048c556b20226679
Jamie Heiney <jheiney10@gmail.com>
2021-03-22 11:40:09 -0400

    "Added sound to animation"

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 
 
+
+// Generate Yes or No
 function generate() {
 	
 	let word;
@@ -14,19 +16,40 @@ function generate() {
 }
 
 
+// Add sound
+function sound(src) {
+  this.sound = document.createElement("audio");
+  this.sound.src = src;
+  this.sound.setAttribute("preload", "auto");
+  this.sound.setAttribute("controls", "none");
+  this.sound.style.display = "none";
+  document.body.appendChild(this.sound);
+  this.play = function(){
+    this.sound.play();
+  }
+  this.stop = function(){
+    this.sound.pause();
+  }
+}
+
+
+// Apply Yes or No to display box and animate
+var clickSound;
+mySound = new sound("ding.mp3");
+
+
 function display() {
+	mySound.play()
+
 	let answer = generate();
-
+	
 	let x = document.getElementsByClassName('display');
-
 	x[0].innerText = answer;
-
-
 	x[0].classList.remove('animate');
 	
 	setTimeout(function () {
   	x[0].classList.add('animate');
 	}, 10);
-	
+
 }
 
